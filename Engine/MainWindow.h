a55fa2315c4313faeb03c4911d29b6a6cbc1518d
DicheBach <dichebach@gmail.com>
2018-01-31 19:19:25 -0500

    changed physical properties of game entities (dude and poo) to use floats instead of ints

--- Engine/MainWindow.h
+++ Engine/MainWindow.h
@@ -17,6 +17,7 @@
 *																						  *
 *	You should have received a copy of the GNU General Public License					  *
 *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+Change to commit to my own online git repo
 ******************************************************************************************/
 #pragma once
 #include "ChiliWin.h"
